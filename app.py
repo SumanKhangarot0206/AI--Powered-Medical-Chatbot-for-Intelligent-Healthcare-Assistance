4cdeb165abb50ab7b628eeaa9637b1cab8df3cfe
Adi123XD <adarshagarwal932@gmail.com>
2025-04-07 08:00:52 +0530

    flask added

--- app.py
+++ app.py
@@ -49,5 +49,7 @@ def home():
         return render_template("index.html", response=response, query=user_query)
     return render_template("index.html", response=None)
 
+# Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = 5000
+    app.run(host="0.0.0.0", port=port)
